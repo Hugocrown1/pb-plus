a28258aa33f68bb56dbe9005413c843d12acd957
Alan <75879852+alanxpo@users.noreply.github.com>
2024-02-26 02:03:54 -0800

    Update account

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -3,28 +3,32 @@ import React from "react";
 import UserProperties from "@/components/UserProperties";
 import SignOutButton from "./SignOutButton";
 import { auth } from "../api/auth/[...nextauth]/route";
+import EditButton from "./EditButton";
 
 const page = async () => {
   const session = await auth();
   return (
-    <main className="relative bg-[#f5f3f4] pt-[90px] min-h-[800px] ">
-      <div className="container-xl mb-12 gap-6">
-        <section className="flex flex-row gap-4">
+    <main className="relative bg-[#f5f3f4] pt-[60px] min-h-[800px] ">
+      <div className="container-xl mb-12 gap-6 bg-white p-8">
+        <section className="flex flex-row gap-4 ">
           <img
             src={session?.user?.image}
-            width={150}
-            height={150}
-            className="rounded-full aspect-square"
+            width={220}
+            height={220}
+            className="rounded-md aspect-square"
             alt="Imagen de usuario"
           />
-          <div className="flex flex-col items-start">
-            <h1>{session?.user.name}</h1>
-            <SignOutButton />
+          <div className="flex flex-col self-center">
+            <h2 className="text">{session?.user.name}</h2>
+            <div className="flex gap-1">
+          <EditButton />
+          <SignOutButton />
+        </div>
           </div>
         </section>
-
-        <section className="flex flex-col gap-4">
-          <h2 className="font-semibold text-left">Published properties</h2>
+        
+        <h2 className="text-left text-2xl">Published properties</h2>
+        <section className="flex flex-col bg-[#f5f3f4] rounded-md mx-auto">
           <UserProperties user={session?.user} />
         </section>
 
