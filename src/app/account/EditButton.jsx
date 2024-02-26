dbc7e532d7c68fb4b9cbbed78951f10b001854cb
Alan <75879852+alanxpo@users.noreply.github.com>
2024-02-26 02:04:12 -0800

    Create EditButton.jsx

---
+++ src/app/account/EditButton.jsx
@@ -0,0 +1,16 @@
+"use client";
+import { signOut } from "next-auth/react";
+import React from "react";
+
+const EditButton = () => {
+  return (
+    <button
+      onClick={""}
+      className="primary-button hover:bg-gray-500/10 transition-colors"
+    >
+      Edit Profile
+    </button>
+  );
+};
+
+export default EditButton;
