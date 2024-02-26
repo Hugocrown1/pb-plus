a0c41ec64fd07603b7cec46f26c934f999ddf2a1
Alan <75879852+alanxpo@users.noreply.github.com>
2024-02-26 02:08:09 -0800

    Update UserProperties

--- src/components/UserProperties.jsx
+++ src/components/UserProperties.jsx
@@ -2,10 +2,10 @@ import users from "@/lib/users";
 import PropertyCard from "./PropertyCard";
 
 const UserProperties = async ({ user }) => {
-  const { properties } = await users.getUser(user?.id);
+  const { properties } = await users.getUser(user.id);
 
   return (
-    <div className=" flex-wrap ">
+    <div className="grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-2 self-start">
       {properties.map((property) => (
         <PropertyCard key={property._id} {...property} />
       ))}
