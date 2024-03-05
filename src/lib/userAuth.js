664bf802357b89a42a4fccfa0e83fabd96a2d9b1
DiegoRB2001 <al19760621@ite.edu.mx>
2024-03-05 11:21:04 -0800

    Correcciones a proteccion de endpoints

--- src/lib/userAuth.js
+++ src/lib/userAuth.js
@@ -1,9 +1,11 @@
 const session = await getServerSession(authOptions);
 
 export const verifyUser = (id) => {
+  if (session === null) return false;
   return id === session.user.id || session.user.role === "admin";
 };
 
 export const isAdmin = () => {
+  if (session === null) return false;
   return session.user.role === "admin";
 };
