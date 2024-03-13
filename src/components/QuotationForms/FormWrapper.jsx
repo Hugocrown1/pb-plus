78706e4b448bd2204ea4628e07cae2723803b76b
Hugo Corona <hugo.corona.r@gmail.com>
2024-03-12 18:56:14 -0700

    Responsividad servicios y formulario remo

--- src/components/QuotationForms/FormWrapper.jsx
+++ src/components/QuotationForms/FormWrapper.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const FormWrapper = ({ title, children }) => {
   return (
     <>
-      <h2 className="text-2xl text-center w-full mb-[40px]">{title}</h2>
+      <h2 className="text-xl min-[395px]:text-2xl text-center w-full mb-[40px]">
+        {title}
+      </h2>
       <div className="px-6">{children}</div>
     </>
   );
