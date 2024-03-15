ba5054fd6c11384aad6dc608dcc2ff473f77d4e8
Alan <75879852+alanxpo@users.noreply.github.com>
2024-03-15 02:41:23 -0700

    Update route.js

--- src/app/api/pricing/[id]/route.js
+++ src/app/api/pricing/[id]/route.js
@@ -1,1 +1,24 @@
+import { connectDB } from "@/lib/mongoose";
+import Consultations from "@/models/consultations";
 
+import { NextResponse } from "next/server";
+
+export async function DELETE(request,{params: {id}}) {
+  try {
+    await connectDB();
+
+    // Buscar y eliminar el usuario por su id
+    const deletedForm = await Consultations.findByIdAndDelete(id);
+
+    if (!deletedForm) {
+      return NextResponse.json("Form no encontrado.", {
+        status: 404,
+      });
+    }
+
+    return NextResponse.json("Form eliminado correctamente.", { status: 200 });
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json(error.message, { status: 500 });
+  }
+}
