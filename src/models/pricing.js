a7407b69fe65a11185fd343f78717c09f42a5a0f
Alan <75879852+alanxpo@users.noreply.github.com>
2024-03-15 02:38:54 -0700

    Update pricing.js

--- src/models/pricing.js
+++ src/models/pricing.js
@@ -1,1 +1,12 @@
+import mongoose, { models } from "mongoose";
 
+const schema = new mongoose.Schema({
+  serviceName: String,
+  userName: String,
+  userPhone: Number,
+  userEmail: String,
+  date: Date,
+  message: String,
+});
+
+export default models?.Pricing || mongoose.model("Pricing", schema);
