2d2e6af76266e13c1a3f52b537aa16148f983de9
Alan <75879852+alanxpo@users.noreply.github.com>
2024-03-15 13:03:39 -0700

    Create layout.jsx

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,80 @@
+"use client"
+import Sidebar, { SidebarItem } from "@/components/Sidebar";
+import { IconDashboard, IconGraph, IconHomeCog, IconGavel, IconUserEdit, IconHomeEdit, IconClockEdit } from "@tabler/icons-react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function Layout({ user, admin }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  
+  const isAdmin = session?.user?.role === "admin";
+  
+  const isUser = session?.user?.role === "user";
+
+  
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  if (!session) {
+    router.replace("/");
+    return null;
+  }
+
+  if (!isAdmin && !isUser) {
+    router.replace("/");
+    return null;
+  }
+
+  return (
+    <div className={isAdmin ? "flex" : ""}>
+       {isAdmin && (
+        <Sidebar>
+          <p className="text-gray-500 font bold my-2">MENU</p>
+          <SidebarItem
+            href={"/dashboard"}
+            icon={<IconDashboard size={20} />}
+            text="Dashboard"
+          />
+          <SidebarItem
+            href={"/dashboard/analytics"}
+            icon={<IconGraph size={20} />}
+            text="Analytics"
+          />
+          <p className="text-gray-500 font bold my-2">REMO</p>
+          <SidebarItem
+            href={"/dashboard/remo"}
+            icon={<IconHomeCog size={20} />}
+            text="Remo Quotes"
+          />
+           <p className="text-gray-500 font bold my-2">LEGAL</p>
+          <SidebarItem
+            href={"/dashboard/forms"}
+            icon={<IconGavel size={20} />}
+            text="Legal Forms"
+          />
+          <p className="text-gray-500 font bold my-2">EDITOR</p>
+          <SidebarItem
+            href={"/dashboard/users"}
+            icon={<IconUserEdit size={20} />}
+            text="Users"
+          />
+          <SidebarItem
+            href={"/dashboard/properties"}
+            icon={<IconHomeEdit size={20} />}
+            text="Properties"
+          />
+          <SidebarItem
+            href={"/dashboard/events"}
+            icon={<IconClockEdit size={20} />}
+            text="Events"
+          />
+          
+         
+        </Sidebar>
+      )}
+      {isAdmin ? admin : user}
+    </div>
+  );
+}
