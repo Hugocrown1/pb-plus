0ae3c9d98678b4326b705744a32876f10cb4585b
DiegoRB2001 <al19760621@ite.edu.mx>
2024-04-02 13:30:35 -0700

    Diseño de información de eventos

--- src/lib/events.js
+++ src/lib/events.js
@@ -1,9 +1,15 @@
 import axios from "axios";
+import { cache } from "react";
 
-const BASE_URL = process.env.URL + "/api/events";
+const BASE_URL = process.env.URL + "/api/events/";
 
-export const getEvents = async () => {
-  const res = await axios(BASE_URL);
+export const getEvents = cache(async () => {
+  const response = await axios(BASE_URL);
 
-  return res.data;
-};
+  return response.data;
+});
+
+export const getEvent = cache(async (id) => {
+  const response = await axios.get(BASE_URL + id);
+  return response.data;
+});
