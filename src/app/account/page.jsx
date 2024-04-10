3a44f58219e11c39a12ab1f456fc35696545c81c
Hugo Corona <hugo.corona.r@gmail.com>
2024-04-09 22:13:37 -0700

    Refactor de account

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -1,98 +1,68 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 import UserProperties from "@/components/UserProperties";
 import SignOutButton from "./SignOutButton";
 import Image from "next/image";
-import { useSession } from "next-auth/react";
+import { auth } from "../api/auth/[...nextauth]/route";
+import Users from "@/models/users";
+import { connectDB } from "@/lib/mongoose";
 import UserEditForm from "@/components/UserEdit/UserEditForm";
-import EditButton from "./EditButton";
-import LoadingScreen from "@/components/LoadingScreen/LoadingScreen";
 
-const Page = () => {
-  const { data: session, status } = useSession();
+import { Stripe } from "stripe";
+import { createCustomerIfNull, generateCustomerPortalLink } from "@/lib/stripe";
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const getUser = async (id) => {
+  await connectDB();
+  const response = await Users.findById(id).populate("properties", {});
+
+  return response.toJSON();
+};
+
+const Page = async () => {
+  const session = await auth();
+
   const defaultImage = "/assets/defaultprofile.jpg";
-  const [loading, setLoading] = useState(true);
-  const [showEditForm, setShowEditForm] = useState(false);
-  const [selectedUserId, setSelectedUserId] = useState(null);
-  const [userProperties, setUserProperties] = useState(null);
-  const [userName, setUserName] = useState(null);
-  const [userImage, setUserImage] = useState(null);
 
-  useEffect(() => {
-    if (session && session.user) {
-      axios
-        .get(`/api/users/${session.user.id}`)
-        .then((response) => {
-          setUserImage(response.data.image);
-          setUserName(response.data.name.toLowerCase());
-          setUserProperties(response.data.properties);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error("Error fetching user data:", error);
-          setLoading(false);
-        });
-    }
-  }, [session, showEditForm]);
+  const user = await getUser(session?.user?.id);
 
-  const openEditForm = (userId) => {
-    setSelectedUserId(userId);
-    setShowEditForm(true);
-  };
+  // await createCustomerIfNull();
 
-  const closeEditForm = () => {
-    setShowEditForm(false);
-  };
-
-  if (loading) {
-    return (
-      <div className="h-screen">
-        <LoadingScreen />
-      </div>
-    );
-  }
+  // const manage = await generateCustomerPortalLink(""+ )
 
   return (
     <main className="relative bg-[#f5f3f4] pt-[60px] min-h-[800px]">
       <div className="container-xl mb-16 gap-6 bg-white p-8 h-full border border-gray-200">
         <section className="flex flex-row gap-4 ">
           <Image
-            src={userImage || defaultImage}
+            src={user.image || defaultImage}
             width={225}
             height={225}
             className="rounded-xl aspect-square border-4 border-gray-200"
             alt="Imagen de usuario"
           />
           <div className="flex flex-col self-center">
-            <h2 className="text-2xl text-left capitalize">{userName}</h2>
+            <h2 className="text-2xl text-left capitalize">{user.name}</h2>
             <div className="flex gap-1"></div>
           </div>
         </section>
         <div className="flex  sm:justify-start justify-evenly">
-          <EditButton onClick={() => openEditForm(session?.user?.id)} />
+          <UserEditForm />
           <SignOutButton />
         </div>
-
         <h2 className="text-left text-2xl">Published properties</h2>
         <section className="flex flex-col rounded-md mx-auto">
-          {userProperties && userProperties.length === 0 ? (
+          {!user.properties ? (
             <p className="text-left text-gray-700">
               No properties published yet
             </p>
           ) : (
-            <UserProperties data={userProperties} />
+            <UserProperties data={user?.properties} />
           )}
         </section>
-
         <h2 className="text-left text-2xl">Events</h2>
         <section className="flex flex-col gap-4 mx-auto">
           <p className="text-left text-gray-700">No events published yet</p>
         </section>
       </div>
-      {showEditForm && (
-        <UserEditForm userId={selectedUserId} onClose={closeEditForm} />
-      )}
     </main>
   );
 };
--- src/components/UserEdit/UserEditForm.jsx
+++ src/components/UserEdit/UserEditForm.jsx
@@ -1,8 +1,16 @@
+"use client";
+
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { IconX, IconPhotoEdit } from "@tabler/icons-react";
+import EditButton from "./EditButton";
+import { useSession } from "next-auth/react";
 
-const UserEditForm = ({ userId, onClose }) => {
+const UserEditForm = () => {
+  const { data: session } = useSession();
+
+  const userId = session?.user.id;
+
   const defaultImage = "/assets/defaultprofile.jpg";
   const [userData, setUserData] = useState({});
   const [error, setError] = useState(null);
@@ -10,6 +18,8 @@ const UserEditForm = ({ userId, onClose }) => {
   const [showUpdateConfirmation, setShowUpdateConfirmation] = useState(false);
   const [previewImage, setPreviewImage] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const [showEditForm, setShowEditForm] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,7 +34,9 @@ const UserEditForm = ({ userId, onClose }) => {
       setLoading(false);
     };
 
-    fetchData();
+    if (userId) {
+      fetchData();
+    }
   }, [userId]);
 
   const handleChange = (e) => {
@@ -50,7 +62,7 @@ const UserEditForm = ({ userId, onClose }) => {
       }
 
       await axios.put(`/api/users/${userId}`, updatedUserData);
-      onClose();
+      setShowEditForm(false);
     } catch (error) {
       setError("Failed to update user");
     }
@@ -81,155 +93,157 @@ const UserEditForm = ({ userId, onClose }) => {
   };
 
   return (
-    <div className="fixed top-0  z-50 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center">
-      {loading && (
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-md shadow-lg">
-          <div className="flex justify-center items-center">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-red-500"></div>
-          </div>
-        </div>
-      )}
-      <div className="bg-white p-6 rounded-md shadow-lg w-[50vh] flex flex-col items-end">
-        <button
-          onClick={onClose}
-          className="text-black hover:bg-red-400 hover:text-white rounded-full p-1"
-        >
-          <IconX></IconX>
-        </button>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            setShowUpdateConfirmation(true);
-          }}
-          className="space-y-4"
-        >
-          <input type="hidden" name="id" value={userId} />
-          <div className="flex justify-center items-center space-x-4 mb-4">
-            <div className="relative inline-block">
-              <img
-                src={
-                  previewImage
-                    ? previewImage
-                    : userData.image
-                    ? userData.image
-                    : defaultImage
-                }
-                alt="Profile"
-                className="h-44 w-44 rounded-xl border-4 border-gray-200"
-                onClick={() => document.getElementById("image").click()}
-                title="Edit"
-              />
-              <div
-                onClick={() => document.getElementById("image").click()}
-                className="absolute inset-0 flex items-center justify-center w-full h-full opacity-0 hover:opacity-50 bg-black text-white rounded-xl cursor-pointer"
-              >
-                <span className="text-lg font-bold">
-                  <IconPhotoEdit size={60} />
-                </span>
+    <>
+      <EditButton onClick={() => setShowEditForm(true)} />
+      {showEditForm && (
+        <div className="fixed top-0  z-50 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center">
+          {loading && (
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-md shadow-lg">
+              <div className="flex justify-center items-center">
+                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-red-500"></div>
               </div>
             </div>
-            <input
-              type="file"
-              accept="image/*"
-              id="image"
-              style={{ display: "none" }}
-              onChange={handleImageChange}
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={userData.name || ""}
-              onChange={handleChange}
-              placeholder="Name"
-              className="w-full px-3 py-2 border rounded-md border-gray-200 bg-gray-100 text-gray-900"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={userData.email || ""}
-              onChange={handleChange}
-              placeholder="Email"
-              className="w-full px-3 py-2 border rounded-md border-gray-200 bg-gray-100 text-gray-900 "
-              required
-            />
-          </div>
-
-          
-          <div>
-            <label
-              htmlFor="phone"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Phone
-            </label>
-            <input
-              type="text"
-              name="phone"
-              id="phone"
-              value={userData.phone || ""}
-              onChange={handleChange}
-              placeholder="Phone"
-              className="w-full px-3 py-2 border rounded-md border-gray-200 bg-gray-100 text-gray-900 "
-              required
-            />
-          </div>
-
-          <div className="flex justify-center">
+          )}
+          <div className="bg-white p-6 rounded-md shadow-lg w-[50vh] flex flex-col items-end">
             <button
-              type="submit"
-              className="bg-blue-200 text-blue-500 hover:text-white px-4 py-2 rounded-md shadow-md hover:bg-blue-500"
+              onClick={() => setShowEditForm(false)}
+              className="text-black hover:bg-red-400 hover:text-white rounded-full p-1"
             >
-              Update
+              <IconX></IconX>
             </button>
-          </div>
-        </form>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                setShowUpdateConfirmation(true);
+              }}
+              className="space-y-4"
+            >
+              <input type="hidden" name="id" value={userId} />
+              <div className="flex justify-center items-center space-x-4 mb-4">
+                <div className="relative inline-block">
+                  <img
+                    src={
+                      previewImage
+                        ? previewImage
+                        : userData.image
+                        ? userData.image
+                        : defaultImage
+                    }
+                    alt="Profile"
+                    className="h-44 w-44 rounded-xl border-4 border-gray-200"
+                    onClick={() => document.getElementById("image").click()}
+                    title="Edit"
+                  />
+                  <div
+                    onClick={() => document.getElementById("image").click()}
+                    className="absolute inset-0 flex items-center justify-center w-full h-full opacity-0 hover:opacity-50 bg-black text-white rounded-xl cursor-pointer"
+                  >
+                    <span className="text-lg font-bold">
+                      <IconPhotoEdit size={60} />
+                    </span>
+                  </div>
+                </div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  id="image"
+                  style={{ display: "none" }}
+                  onChange={handleImageChange}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={userData.name || ""}
+                  onChange={handleChange}
+                  placeholder="Name"
+                  className="w-full px-3 py-2 border rounded-md border-gray-200 bg-gray-100 text-gray-900"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={userData.email || ""}
+                  onChange={handleChange}
+                  placeholder="Email"
+                  className="w-full px-3 py-2 border rounded-md border-gray-200 bg-gray-100 text-gray-900 "
+                  required
+                />
+              </div>
 
-        {error && <p>{error}</p>}
-      </div>
-      {/* Update Confirmation Modal */}
-      {showUpdateConfirmation && (
-        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
-          <div className="bg-white p-6 rounded-md shadow-lg border border-gray-200">
-            <p>Are you sure you want to update?</p>
-            <div className="flex justify-end mt-4">
-              <button
-                onClick={() => {
-                  setShowUpdateConfirmation(false);
-                }}
-                className="bg-gray-200 text-gray-500 hover:text-white hover:bg-gray-500 px-4 py-2 mr-2 rounded-md"
-              >
-                Cancel
-              </button>
-              <button
-                onClick={handleUpdateConfirmation}
-                className="bg-red-200 text-red-500 hover:bg-red-500 hover:text-white px-4 py-2 rounded-md"
-              >
-                Confirm
-              </button>
-            </div>
+              <div>
+                <label
+                  htmlFor="phone"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Phone
+                </label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  value={userData.phone || ""}
+                  onChange={handleChange}
+                  placeholder="Phone"
+                  className="w-full px-3 py-2 border rounded-md border-gray-200 bg-gray-100 text-gray-900 "
+                  required
+                />
+              </div>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="bg-blue-200 text-blue-500 hover:text-white px-4 py-2 rounded-md shadow-md hover:bg-blue-500"
+                >
+                  Update
+                </button>
+              </div>
+            </form>
+            {error && <p>{error}</p>}
           </div>
+          {/* Update Confirmation Modal */}
+          {showUpdateConfirmation && (
+            <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
+              <div className="bg-white p-6 rounded-md shadow-lg border border-gray-200">
+                <p>Are you sure you want to update?</p>
+                <div className="flex justify-end mt-4">
+                  <button
+                    onClick={() => {
+                      setShowUpdateConfirmation(false);
+                    }}
+                    className="bg-gray-200 text-gray-500 hover:text-white hover:bg-gray-500 px-4 py-2 mr-2 rounded-md"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={handleUpdateConfirmation}
+                    className="bg-red-200 text-red-500 hover:bg-red-500 hover:text-white px-4 py-2 rounded-md"
+                  >
+                    Confirm
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       )}
-    </div>
+    </>
   );
 };
 
