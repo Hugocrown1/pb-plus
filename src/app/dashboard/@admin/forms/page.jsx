26c20b3ff3622be11ef58e3d2f236f35877f3670
Alan <75879852+alanxpo@users.noreply.github.com>
2024-04-10 16:32:41 -0700

    Update page.jsx

--- src/app/dashboard/@admin/forms/page.jsx
+++ src/app/dashboard/@admin/forms/page.jsx
@@ -1,41 +1,42 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import FormsTable from "@/components/AdminTables/FormsTable";
+import axios from "axios";
 import Breadcrumb from "@/components/Breadcrumb";
 
 const page = () => {
-    const [consultations, setConsultations] = useState([])
+  const [consultations, setConsultations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [fetchTrigger, setFetchTrigger] = useState(false);
 
-    useEffect(() => {
-        const fetchConsultations= async () => {
-          try {
-            const res = await axios.get("/api/consultations");
-            setConsultations(res.data);
-          } catch (error) {
-            console.error("Error fetching consultations:", error);
-          }
-        };
-    
-        fetchConsultations();
-    
-        
-        const interval = setInterval(fetchConsultations, 5000);
-    
-        return () => {
-          clearInterval(interval);
-        };
-      }, []);
-    
+  useEffect(() => {
+    axios
+      .get("/api/consultations")
+      .then((res) => {
+        setConsultations(res.data.reverse());
+        setLoading(false);
+        setFetchTrigger(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching prices:", error);
+        setLoading(false);
+        setFetchTrigger(false);
+      });
+  }, [fetchTrigger]);
+
   return (
-    <main className="flex w-full">
-    <div className=" w-full pt-[60px] m-4">
-      <div className="w-full  h-20 bg-white shadow-md border border-gray-200 flex justify-start items-center px-4 rounded-xl mb-4">
-      <Breadcrumb />
-      </div>
+    <main className="flex w-full bg-white min-h-screen">
+      <div className="w-full pt-[60px]">
+        <div className="w-full h-16 border-b-4 border-gray-200 flex justify-start items-center xl:p-6 px-4">
+          <Breadcrumb />
+        </div>
 
-      <div className="border border-gray-200 shadow-md bg-white rounded-xl py-2">
-          <FormsTable formData={consultations} />
+        <div className="xl:p-4 px-2">
+          <FormsTable
+            formData={consultations}
+            loading={loading}
+            setFetchTrigger={setFetchTrigger}
+          />
         </div>
       </div>
     </main>
