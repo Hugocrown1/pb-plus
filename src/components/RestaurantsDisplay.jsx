669808dc7a193b2e109581b9bb508dfab1eae4cd
Hugo Corona <hugo.corona.r@gmail.com>
2024-04-30 21:14:47 -0700

    Refactor advertising and events pages, and update components

--- src/components/RestaurantsDisplay.jsx
+++ src/components/RestaurantsDisplay.jsx
@@ -3,6 +3,7 @@ import Events from "@/models/events";
 import RestaurantCard from "./RestaurantCard";
 import Restaurants from "@/models/restaurants";
 import { getAllSubscriptions } from "@/lib/stripe";
+import { IconFileSad } from "@tabler/icons-react";
 
 const getRestaurants = async () => {
   await connectDB();
@@ -31,20 +32,33 @@ export default async function RestaurantsDisplay() {
   );
 
   return (
-    <div className="grid lg:grid-cols-3 min-[677px]:grid-cols-2 grid-cols-1 items-center xl:gap-4 gap-2 justify-items-center">
-      {restaurantsWithActiveSubscriptions.map((item) => {
-        const formattedRestaurant = {
-          ...item,
-          _id: item._id.toString(),
-          user: item.user._id.toString(),
-        };
-        return (
-          <RestaurantCard
-            key={formattedRestaurant._id}
-            {...formattedRestaurant}
-          />
-        );
-      })}
-    </div>
+    <>
+      {restaurantsWithActiveSubscriptions.length === 0 && (
+        <div className="flex flex-col items-center justify-center  w-full h-[876px] text-gray-600 text-center">
+          <IconFileSad size={70} />
+          <p className="font-medium text-3xl text-wrap">
+            No advertisements found.
+          </p>
+        </div>
+      )}
+
+      {!!restaurantsWithActiveSubscriptions.length && (
+        <div className="grid lg:grid-cols-3 min-[677px]:grid-cols-2 grid-cols-1 items-center xl:gap-4 gap-2 justify-items-center">
+          {restaurantsWithActiveSubscriptions.map((item) => {
+            const formattedRestaurant = {
+              ...item,
+              _id: item._id.toString(),
+              user: item.user._id.toString(),
+            };
+            return (
+              <RestaurantCard
+                key={formattedRestaurant._id}
+                {...formattedRestaurant}
+              />
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 }
