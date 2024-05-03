73e558ad54eb195b81d05b50819db0b63a1f7286
DiegoRB2001 <al19760621@ite.edu.mx>
2024-05-02 19:28:27 -0700

    Implementado formulario de información básica de los restaurantes para la lógica de la creación y edición de páginas de restaurantes y cambios en el diseño

---
+++ src/components/RestaurantForm.jsx
@@ -0,0 +1,138 @@
+"use client";
+
+import FormInput from "@/components/FormInput";
+import Link from "next/link";
+import FormFileInput from "./FormFileInput";
+
+const RestaurantForm = ({
+  values,
+  previewImages,
+  onChange,
+  uploadPreviewImage,
+  setShowInitialForm,
+  setValues,
+  isFormOpen,
+  _id,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowInitialForm(false);
+  };
+
+  return (
+    <div
+      className={`pt-[50px] fixed z-40 w-full h-full bg-zinc-950/30 ${
+        !isFormOpen && "invisible"
+      }`}
+    >
+      <div className="flex flex-col xl:flex-row w-full gap-10 my-5 justify-center ">
+        <section className="w-1/2 flex flex-col mt-4 bg-[#f5f3f4] p-5 rounded-md">
+          <h1 className="text-left text-[42px]">
+            {_id ? "Edit restaurant" : "New restaurant"}
+          </h1>
+          <div className="bg-white w-full flex p-4 shadow-md rounded-xl">
+            <form
+              onSubmit={handleSubmit}
+              className="grid grid-cols-2 w-full gap-5"
+            >
+              <div>
+                <h3>Basic information</h3>
+                <FormInput
+                  label="Restaurant name"
+                  name="name"
+                  id="name"
+                  maxLength="100"
+                  placeholder={"Name"}
+                  value={values.name}
+                  onChange={onChange}
+                  errorMessage="Please provide a name"
+                />
+                <label htmlFor="profileImage">Restaurant Image</label>
+                <div className=" h-[360px] relative rounded-md overflow-hidden bg-[#f5f3f4]">
+                  <FormFileInput
+                    alt={"Restaurant profile image"}
+                    id={"profileImage"}
+                    inputColor={"#E7E1E4"}
+                    name={"Profile"}
+                    onChange={uploadPreviewImage}
+                    url={previewImages.Profile}
+                  />
+                </div>
+                <div className="form-input">
+                  <label htmlFor="type">Restaurant category</label>
+                  <select
+                    name="category"
+                    id="category"
+                    defaultValue={values.category}
+                    onChange={onChange}
+                    required
+                  >
+                    <option disabled value={""}>
+                      Choose a category
+                    </option>
+                    <option value={"Restaurant"}>Restaurant</option>
+                    <option value={"Bar"}>Bar</option>
+                    <option value={"Shop"}>Shop</option>
+                    <option value={"Other"}>Other</option>
+                  </select>
+                </div>
+              </div>
+              <div>
+                <h3>Restaurant details</h3>
+                <FormInput
+                  label="Address"
+                  name="address"
+                  id="address"
+                  placeholder={"Restaurant address"}
+                  value={values.address}
+                  onChange={onChange}
+                  errorMessage="Please provide an address"
+                />
+                <hr className="stroke-slate-600 my-4" />
+                <h3>Social medias</h3>
+                {Object.entries(values.socialMedia).map(
+                  ([socialMediaName, socialMediaValue]) => (
+                    <FormInput
+                      key={socialMediaName}
+                      label={socialMediaName}
+                      name={socialMediaName}
+                      id={socialMediaName}
+                      placeholder={socialMediaName}
+                      value={socialMediaValue}
+                      onChange={(e) =>
+                        setValues({
+                          ...values,
+                          socialMedia: {
+                            ...values.socialMedia,
+                            [socialMediaName]: e.target.value,
+                          },
+                        })
+                      }
+                    />
+                  )
+                )}
+              </div>
+              <div className="flex w-full justify-between items-end mt-2 col-span-2">
+                <Link
+                  href={
+                    _id
+                      ? "/community/advertising/" + _id
+                      : "/community/advertising"
+                  }
+                  className="community-button-outline"
+                >
+                  Cancel
+                </Link>
+                <button type="submit" className="community-button">
+                  Continue
+                </button>
+              </div>
+            </form>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantForm;
